efe1659aa1b6858d4a73350378b988cae150efdc
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-09 23:06:05 +0200

    Componentes e páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton/Index";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
