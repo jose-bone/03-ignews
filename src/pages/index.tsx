b00f9657722550c689334da55a4f42679866e5e9
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-09 23:05:37 +0200

    Componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,10 @@ export default function Home() {
             News about the <span>React</span> world.
           </h1>
           <p>
-            Get access to all the publication <br />
-            <span>for $9.90 monthly</span>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
