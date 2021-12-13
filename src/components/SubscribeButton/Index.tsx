bc8daa4dc165e5c09f4cc6b4a816ca76f78cec4b
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-13 21:52:29 +0200

    Redirecionando para o Stripe

--- src/components/SubscribeButton/Index.tsx
+++ src/components/SubscribeButton/Index.tsx
@@ -7,10 +7,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
 
