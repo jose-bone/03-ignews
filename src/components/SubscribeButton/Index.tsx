f4fb1161105f7d3f6a849e8d5aaf39aea8f14322
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-13 21:00:02 +0200

    Gerando sessão de chackout

--- src/components/SubscribeButton/Index.tsx
+++ src/components/SubscribeButton/Index.tsx
@@ -5,8 +5,21 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
