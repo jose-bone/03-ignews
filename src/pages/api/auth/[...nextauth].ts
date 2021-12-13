8f3d7562b21ea23bcdee0a6491142b64f1e1b354
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-13 20:15:57 +0200

    Usando FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -14,6 +18,7 @@ export default NextAuth({
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user;
+
       try {
         await fauna.query(
           q.If(
