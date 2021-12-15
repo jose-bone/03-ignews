c10d178a1ddaed7f67ecb78f871c34e0c705304a
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-16 01:57:38 +0200

    Componente: ActiveLink

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -7,12 +7,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
