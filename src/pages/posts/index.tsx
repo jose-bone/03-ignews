8fd59642846967b2b7fddabf49578e0e47b03c53
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-15 23:24:08 +0200

    Página Posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>15 de Dezembro de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              👋🏾 Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React
+              usando Leaflet.
+            </p>
+          </a>
+          <a href="#">
+            <time>15 de Dezembro de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              👋🏾 Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React
+              usando Leaflet.
+            </p>
+          </a>
+          <a href="#">
+            <time>15 de Dezembro de 2021</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>
+              👋🏾 Neste post vamos desenvolver uma página web para demonstrar, na
+              prática, a integração de Mapas em uma aplicação com React
+              usando Leaflet.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
