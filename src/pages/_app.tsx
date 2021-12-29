b58d33125f5a96c992cdcafbc3153cb44729f7dd
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-12-29 14:27:40 +0200

    Another One 🛠

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import { Header } from "../components/Header/";
+import { Header } from "../components/Header";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 import "../styles/global.scss";
